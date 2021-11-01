177e3fc04e452abcd1d935a6e9105259e46fc263
msgreentea <msgreentea8@gmail.com>
2021-11-01 16:38:12 +0900

    paginateまで、でも404になった

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -9,59 +9,7 @@ class AuthorController extends Controller
 {
     public function index()
     {
-        $items = Author::all();
+        $items = Author::simplePaginate(4);
         return view('index', ['items' => $items]);
-    }
-    public function find()
-    {
-        return view('find', ['input' => '']);
-    }
-    public function search(Request $request)
-    {
-        $item = Author::where('name', 'LIKE', "%{$request->input}%")->first();
-        $param = [
-            'input' => $request->input,
-            'item' => $item
-        ];
-        return view('find', $param);
-    }
-    public function bind(Author $author)
-    {
-        $data = [
-            'item' => $author,
-        ];
-        return view('author.binds', $data);
-    }
-    public function add()
-    {
-        return view('add');
-    }
-    public function create(Request $request)
-    {
-        $this->validate($request, Author::$rules);
-        $form = $request->all();
-        Author::create($form);
-        return redirect('/');
-    }
-    public function edit(Request $request)
-    {
-        $author = Author::find($request->id);
-        return view('edit', ['form' => $author]);
-    }
-    public function update(Request $request)
-    {
-        $this->validate($request, Author::$rules);
-        $form = $request->all();
-        unset($from['_token']);
-        Author::where('id', $request->id)->update($form);
-        return redirect('/');
-    }
-    public function relate(Request $request)
-    {
-        // $items = Author::all();
-        $hasItems = Author::has('book')->get();
-        $noItems = Author::doesntHave('book')->get();
-        $param = ['hasItems' => $hasItems, 'noItems' => $noItems];
-        return view('author.index', $param);
     }
 }
